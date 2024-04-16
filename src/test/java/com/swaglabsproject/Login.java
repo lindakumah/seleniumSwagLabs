dc9a18d026472be4f8a56466fc5805701f9d7a7e
Linda Kumah <79393931+lindakumah@users.noreply.github.com>
2024-04-16 16:38:57 +0000

    First commit

---
+++ src/test/java/com/swaglabsproject/Login.java
@@ -0,0 +1,22 @@
+package com.swaglabsproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Login {
+    WebDriver driver = new ChromeDriver();
+    @Test
+    void getBaseUrl (){
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+        Assert.assertEquals(driver.findElement(By.className("app_logo")).getText(), "Swag Labs");
+        driver.quit();
+    }
+}
